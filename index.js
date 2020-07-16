fded7a14378f09fbf649116f99748f0f39af271d
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-16 20:30:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+const name = 'Leah';
+let holiday = 'Christmas Eve';
+
+function happyHolidays() {
+  return 'Happy holidays!';
+};
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+};
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+};
+
+
+
+
